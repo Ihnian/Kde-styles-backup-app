dae7570ef123a244ca05dfb69cd423fc7f0e3465
Ihnian <jakub.kaczmarekb@gmail.com>
2026-01-16 16:55:54 +0100

    first commit

---
+++ Main.py
@@ -0,0 +1,45 @@
+import shutil
+import pathlib
+from pathlib import Path
+import sys
+from PySide6.QtWidgets import QApplication, QPushButton, QLabel
+from PySide6.QtCore import Slot
+from PySide6 import QtCore, QtWidgets, QtGui
+# Greetings
+
+class myApp(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("Hello World",
+                                     alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.text)
+
+        self.button.clicked.connect(self.test)
+
+    @QtCore.Slot()
+    def test(self):
+        home_dir = Path.home()
+
+        test = pathlib.Path("/usr/share/plasma/desktoptheme/")
+        dest = pathlib.Path(f"{home_dir}/Testor2")
+        test2 = pathlib.Path(f"{home_dir}/.local/share/plasma/look-and-feel/")
+
+        shutil.copytree(test, dest, dirs_exist_ok=True)
+        shutil.copytree(test2, dest, dirs_exist_ok=True)
+
+        self.text.setText("zrobione ez?")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = myApp()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
+    
